51f0c06e425f9d943cebc878734c8fbf6b111054
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2024-11-04 12:44:55 +0900

    add smm

--- models/detection/RVT/models/detection/recurrent_backbone/__init__.py
+++ models/detection/RVT/models/detection/recurrent_backbone/__init__.py
@@ -6,6 +6,10 @@ from .maxvit_rnn import RNNDetector as MaxViTRNNDetector
 def build_recurrent_backbone(backbone_cfg: DictConfig):
     name = backbone_cfg.name
     if name == 'MaxViTRNN':
+        print('LSTM')
+        return MaxViTRNNDetector(backbone_cfg)
+    if name == 'MaxViTRNN-SSM':
+        print('SSM')
         return MaxViTRNNDetector(backbone_cfg)
     else:
         raise NotImplementedError
